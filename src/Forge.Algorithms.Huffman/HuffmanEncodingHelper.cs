077e85818555e760a59cd1fd2105f6de6e5e8379
Nick Alcorso <nalcorso@gmail.com>
2024-06-07 08:55:03 +1000

    Encoding: Add a separate helper for detecting encoding types

---
+++ src/Forge.Algorithms.Huffman/HuffmanEncodingHelper.cs
@@ -0,0 +1,42 @@
+﻿namespace Forge.Algorithms.Huffman;
+
+public static class HuffmanEncodingHelper
+{
+    public enum EncodingType
+    {
+        Unknown,
+        Bin,
+        Hex,
+        Base64
+    }
+    
+    /// <summary>
+    /// Detects the encoding of the input string.
+    /// </summary>
+    /// <param name="input">The input string to analyse.</param>
+    /// <returns>The detected encoding type.</returns>
+    /// <remarks>This is a weak detection method, as it is possible for the input to be ambiguous. For example, a string
+    /// of all 0s and 1s could be binary or base64 encoded. This method will return the first match it finds in the order
+    /// of Bin, Hex, Base64, Unknown.</remarks>
+    public static EncodingType DetectEncoding(string input)
+    {
+        if (input.All(c => c == '0' || c == '1'))
+        {
+            return EncodingType.Bin;
+        }
+
+        if (input.All(c => (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f')))
+        {
+            return EncodingType.Hex;
+        }
+
+        try
+        {
+            _ = Convert.FromBase64String(input);
+            return EncodingType.Base64;
+        }
+        catch (FormatException) {/* Not base64 */}
+        
+        return EncodingType.Unknown;
+    }
+}
---
+++ tests/HuffmanCoding.UnitTests/HuffmanEncodingHelperTests.cs
@@ -0,0 +1,86 @@
+﻿namespace HuffmanCoding.UnitTests;
+
+public class HuffmanEncodingHelperTests
+{
+    [Theory]
+    [InlineData("01010101", HuffmanEncodingHelper.EncodingType.Bin)]
+    [InlineData("9ABCDEF", HuffmanEncodingHelper.EncodingType.Hex)]
+    [InlineData("SGVsbG8gd29ybGQ=", HuffmanEncodingHelper.EncodingType.Base64)]
+    [InlineData("Hello World", HuffmanEncodingHelper.EncodingType.Unknown)]
+    public void DetectEncoding_ShouldReturnCorrectEncodingType(string input, HuffmanEncodingHelper.EncodingType expected)
+    {
+        // Arrange
+        // No arrangement needed as we are directly passing the inputs to the method
+
+        // Act
+        var result = HuffmanEncodingHelper.DetectEncoding(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void DetectEncoding_ShouldReturnBin_WhenInputIsAmbiguous()
+    {
+        // Arrange
+        var input = "01010101"; // This could be interpreted as Bin or Base64
+
+        // Act
+        var result = HuffmanEncodingHelper.DetectEncoding(input);
+
+        // Assert
+        result.Should().Be(HuffmanEncodingHelper.EncodingType.Bin);
+    }
+    
+    [Fact]
+    public void DetectEncoding_ShouldReturnUnknown_WhenInputIsNull()
+    {
+        // Arrange
+        string input = null;
+
+        // Act
+        var result = HuffmanEncodingHelper.DetectEncoding(input);
+
+        // Assert
+        result.Should().Be(HuffmanEncodingHelper.EncodingType.Unknown);
+    }
+    
+    [Fact]
+    public void DetectEncoding_ShouldReturnUnknown_WhenInputIsEmpty()
+    {
+        // Arrange
+        var input = "";
+
+        // Act
+        var result = HuffmanEncodingHelper.DetectEncoding(input);
+
+        // Assert
+        result.Should().Be(HuffmanEncodingHelper.EncodingType.Unknown);
+    }
+
+    [Fact]
+    public void DetectEncoding_ShouldReturnUnknown_WhenInputIsOnlyPaddingCharacters()
+    {
+        // Arrange
+        var input = "===";
+
+        // Act
+        var result = HuffmanEncodingHelper.DetectEncoding(input);
+
+        // Assert
+        result.Should().Be(HuffmanEncodingHelper.EncodingType.Unknown);
+    }
+
+    [Fact]
+    public void DetectEncoding_ShouldReturnUnknown_WhenInputContainsInvalidCharacters()
+    {
+        // Arrange
+        var input = "Hello World!";
+
+        // Act
+        var result = HuffmanEncodingHelper.DetectEncoding(input);
+
+        // Assert
+        result.Should().Be(HuffmanEncodingHelper.EncodingType.Unknown);
+    }
+}
